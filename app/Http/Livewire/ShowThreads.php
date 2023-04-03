5fcd8d02be41a663af8b4761b5fe7db6514ae21f
Italo Morales F <i@platzi.com>
2023-04-03 14:39:50 -0500

    estructura visual de las preguntas

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire;
 
 use App\Models\Category;
+use App\Models\Thread;
+
 use Livewire\Component;
 
 class ShowThreads extends Component
@@ -10,9 +12,11 @@ class ShowThreads extends Component
     public function render()
     {
         $categories = Category::get();
+        $threads = Thread::latest()->get();
 
         return view('livewire.show-threads', [
-            'categories' => $categories
+            'categories' => $categories,
+            'threads' => $threads,
         ]);
     }
 }
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -22,5 +22,35 @@
             </li>
         </ul>
     </div>
-    <div class="w-full"></div>
+    <div class="w-full">
+        <!-- formulario -->
+
+        @foreach($threads as $thread)
+        <div class="rounded-md bg-gradient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
+            <div class="p-4 flex gap-4">
+                <div>Image, avatar</div>
+                <div class="w-full">
+                    <h2 class="mb-4 flex items-start justify-between">
+                        <a href="" class="text-xl font-semibold text-white/90">
+                            {{ $thread->title }}
+                        </a>
+                        <span 
+                            class="rounded-full text-xs py-2 px-4 capitalize" 
+                            style="color: #00aced; border: 1px solid #00aced;"
+                        >
+                            Categoría
+                        </span>
+                    </h2>
+                    <p class="flex items-center justify-between w-full text-xs">
+                        <span class="text-blue-600 font-semibold">
+                            Usuario
+                            <span class="text-white/90">{{ $thread->created_at->diffForHumans() }}</span>
+                        </span>
+                        <span class="text-slate-700">Respuestas y botón</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
