adef584a1ca11943542cc3d1f2b48416495abfbf
Italo Morales F <i@platzi.com>
2023-04-05 12:32:02 -0500

    campos del formulario

--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -1,3 +1,34 @@
 <div>
-    formulario
+    <select 
+        name="category_id"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4"
+    >
+        <option value="">Seleccionar categoría</option>
+
+        @foreach ($categories as $category)
+            <option 
+                value="{{ $category->id }}"
+
+                @if ($thread->category_id == $category->id)
+                selected
+                @endif
+
+            >{{ $category->name }}</option>
+        @endforeach
+    </select>
+
+    <input 
+        type="text" 
+        name="title" 
+        placeholder="Título"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+        value="{{ $thread->title }}"
+    >
+
+    <textarea 
+        name="body" 
+        rows="10"
+        placeholder="Descripción del problema"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+    >{{ $thread->body }}</textarea>
 </div>
